7407ba8c1795d77aa3d18e397d9093898ef87c3a
Danielborbafs <dev.danielborba@gmail.com>
2023-06-20 19:43:38 -0300

    Exerciciospt2

---
+++ Expressões/expressoes.js
@@ -0,0 +1,6 @@
+/* Exercício 1: Calcule a área de um retângulo com base e altura fornecidas. 
+Atribua o resultado a uma variável chamada area e exiba o valor no console. */ 
+areaRetBase = 40
+areaRetAlt = 20
+areaTotal = 40 * 20
+console.log(areaTotal)
