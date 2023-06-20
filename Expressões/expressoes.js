eca58122d2f4ac7701afefca35e9a7aec3109adf
Danielborbafs <dev.danielborba@gmail.com>
2023-06-20 19:45:07 -0300

    atribuindoVariaveis

--- Expressões/expressoes.js
+++ Expressões/expressoes.js
@@ -1,6 +1,7 @@
 /* Exercício 1: Calcule a área de um retângulo com base e altura fornecidas. 
 Atribua o resultado a uma variável chamada area e exiba o valor no console. */ 
-areaRetBase = 40
-areaRetAlt = 20
-areaTotal = 40 * 20
+const areaRetBase = 40
+const areaRetAlt = 20
+const areaTotal = 40 * 20
 console.log(areaTotal)
+
