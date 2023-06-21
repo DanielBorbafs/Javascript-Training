c75a27226378953d93f57bd0f0c5079071e8ff28
Danielborbafs <dev.danielborba@gmail.com>
2023-06-21 18:45:51 -0300

    Exercicio02

---
+++ Expressões/exercicios-expressoes02.js
@@ -0,0 +1,21 @@
+/* Exercício 1: Verificação de número primo
+Escreva um programa que solicite ao usuário um número inteiro e verifique se ele é um número primo. 
+Armazene o resultado em uma variável booleana e exiba-o no console.
+
+Exercício 2: Cálculo do desconto
+Escreva um programa que solicite ao usuário o valor de um produto e o percentual de desconto a ser aplicado. 
+Calcule o valor do desconto e o preço final do produto com o desconto. Armazene os resultados em variáveis e exiba-os no console.
+
+Exercício 3: Verificação de palíndromo
+Escreva um programa que solicite ao usuário uma palavra ou frase e verifique se ela é um palíndromo
+ (ou seja, pode ser lida da mesma forma tanto da esquerda para a direita quanto da direita para a esquerda, 
+    desconsiderando espaços e diferenciação entre letras maiúsculas e minúsculas). 
+    Armazene o resultado em uma variável booleana e exiba-o no console.
+
+Exercício 4: Cálculo do fatorial
+Escreva um programa que solicite ao usuário um número inteiro positivo e calcule o seu fatorial. 
+Armazene o resultado em uma variável e exiba-o no console.
+
+Exercício 5: Verificação de triângulo
+Escreva um programa que solicite ao usuário as medidas de três lados de um triângulo
+ e verifique se é possível formar um triângulo com essas medidas. Armazene o resultado em uma variável booleana e exiba-o no console.*/
