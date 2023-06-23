ec6ed5144ed1eed93aff6009e1845b45d8718b5f
Danielborbafs <dev.danielborba@gmail.com>
2023-06-22 22:14:58 -0300

    Exercicio01

---
+++ Loops/exercicios-loops.js
@@ -0,0 +1,31 @@
+/* 
+Exercício de contagem ascendente:
+01 - Escreva um loop for que imprima os números de 1 a 10 no console.
+
+Exercício de contagem descendente:
+Escreva um loop for que imprima os números de 10 a 1 no console.
+
+Exercício de soma de elementos:
+Dado um array de números, escreva um loop for que calcule e retorne a soma de todos os elementos no array.
+
+Exercício de multiplicação de elementos:
+Dado um array de números, escreva um loop for que calcule e retorne o produto de todos os elementos no array.
+
+Exercício de impressão de elementos:
+Dado um array de strings, escreva um loop for que imprima cada elemento no console.
+
+Exercício de busca de elemento:
+Dado um array de números, escreva um loop for que verifique se um determinado número está presente no array. Se estiver, imprima "Encontrado!" no console; caso contrário, imprima "Não encontrado!".
+
+Exercício de tabuada:
+Escreva um loop for aninhado para gerar a tabuada de multiplicação de 1 a 10. Imprima cada resultado no formato "2 x 3 = 6".
+
+Exercício de contagem regressiva:
+Escreva um loop for que faça uma contagem regressiva de 10 a 0. Imprima cada número no console e, quando chegar a 0, imprima "Lançamento!".
+
+*/
+
+// Exercicio 01 
+for ( let i = 1; i < 11; i++) {
+    console.log(i)
+}
