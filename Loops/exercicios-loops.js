88fcabbe3df6f6eefd91acb61352fbcc003ed23b
Danielborbafs <dev.danielborba@gmail.com>
2023-06-22 22:51:59 -0300

    exercicio03

--- Loops/exercicios-loops.js
+++ Loops/exercicios-loops.js
@@ -32,11 +32,10 @@ for (let i=10; i >= 1; i--) {
 
 
 // 03- Dado um array de números, escreva um loop for que calcule e retorne a soma de todos os elementos no array.
-let numeros = [1, 2, 3, 4, 5];
+let numeros = [10, 2, 3, 4, 5];
 let soma = 0;
 
 for (let i = 0; i < numeros.length; i++) {
   soma += numeros[i];
 }
-
-console.log(soma); // Output: 15
+console.log(soma);
