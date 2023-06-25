a1c7ed8e3fca0e329cae7d74b34e615703818504
DanielBorbafs <dev.danielborba@gmail.com>
2023-06-25 16:51:59 -0300

    Exercicioifelse

--- If_else/exercicios-ifelse02.js
+++ If_else/exercicios-ifelse02.js
@@ -6,10 +6,6 @@ Escreva um programa que solicite ao usuário um número e verifique se ele é po
 Exercício 2: Verificação de idade:
 Escreva um programa que solicite ao usuário sua idade e exiba uma mensagem dizendo se ele é maior de idade ou menor de idade.
 
-Exercício 3: Verificação de login:
-Crie um programa que solicite ao usuário um nome de usuário e uma senha.
- Verifique se as informações correspondem a um login válido e exiba uma mensagem de boas-vindas 
- se o login for bem-sucedido. Caso contrário, exiba uma mensagem de erro.
 
 Exercício 4: Verificação de número par ou ímpar:
 Crie um programa que solicite ao usuário um número inteiro e exiba uma mensagem dizendo se o número é par ou ímpar.
@@ -20,6 +16,8 @@ e exiba uma mensagem correspondente ao dia da semana (por exemplo, "Domingo", "S
 
 */
 
+
+
 // Exercicio 02
 const idade = prompt("Digite sua idade")
 
@@ -29,6 +27,20 @@ if (idade >= 18 ) {
     console.log("Voce é menor de idade")
 }
 
+/* 
+Exercício 3: Verificação de login:
+Crie um programa que solicite ao usuário um nome de usuário e uma senha.
+ Verifique se as informações correspondem a um login válido e exiba uma mensagem de boas-vindas 
+ se o login for bem-sucedido. Caso contrário, exiba uma mensagem de erro.
+*/
+const login = prompt("Digite seu login")
+const senha = prompt ("Digite sua senha")
+
+if (login==="admin" && senha === "123") {
+    console.log("Boas vindas admin")
+} else {
+    console.log("Informacoes erradas, tente novamente")
+}
 // Exercicio 04
 const numero = prompt("digite um número")
 if ( numero % 2 === 0) {
