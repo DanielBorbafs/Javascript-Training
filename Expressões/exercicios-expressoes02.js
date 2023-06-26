07c59b5e214ec4566c68242874e6435a31e1bd3e
Danielborbafs <dev.danielborba@gmail.com>
2023-06-26 20:13:07 -0300

    exerciciosbases

--- Expressões/exercicios-expressoes02.js
+++ Expressões/exercicios-expressoes02.js
@@ -1,26 +1,9 @@
-/* Exercício 1: Verificação de número primo
-Escreva um programa que solicite ao usuário um número inteiro e verifique se ele é um número primo. 
-Armazene o resultado em uma variável booleana e exiba-o no console.
 
-Exercício 3: Verificação de palíndromo
-Escreva um programa que solicite ao usuário uma palavra ou frase e verifique se ela é um palíndromo
- (ou seja, pode ser lida da mesma forma tanto da esquerda para a direita quanto da direita para a esquerda, 
-    desconsiderando espaços e diferenciação entre letras maiúsculas e minúsculas). 
-    Armazene o resultado em uma variável booleana e exiba-o no console.
 
-Exercício 4: Cálculo do fatorial
-Escreva um programa que solicite ao usuário um número inteiro positivo e calcule o seu fatorial. 
-Armazene o resultado em uma variável e exiba-o no console.
-
-Exercício 5: Verificação de triângulo
-Escreva um programa que solicite ao usuário as medidas de três lados de um triângulo
- e verifique se é possível formar um triângulo com essas medidas. Armazene o resultado em uma variável booleana e exiba-o no console.*/
-
- 
- /* Exercicio 01
+/* Exercicio 01
 Escreva um programa que solicite ao usuário o valor de um produto e o percentual de desconto a ser aplicado. 
 Calcule o valor do desconto e o preço final do produto com o desconto. Armazene os resultados em variáveis e exiba-os no console.
- */
+*/
 
 const produto = parseFloat(prompt("Qual será o valor de venda do produto?"));
 const desconto = parseFloat(prompt("Qual será o valor do desconto?"));
@@ -30,3 +13,27 @@ const precoFinal = produto - valorDesconto;
 
 console.log("O valor do desconto é: R$" + valorDesconto.toFixed(2));
 console.log("O preço final do produto com desconto é: R$" + precoFinal.toFixed(2));
+
+/* Exercicio 02 
+Escreva um programa que solicite um número ao usuário e informe se ele é primo ou não
+*/
+const numeroUsuario = parseInt(prompt("Digite um número"));
+
+let isPrimo = true;
+
+if (numeroUsuario <= 1) {
+  isPrimo = false;
+} else {
+  for (let i = 2; i <= Math.sqrt(numeroUsuario); i++) {
+    if (numeroUsuario % i === 0) {
+      isPrimo = false;
+      break;
+    }
+  }
+}
+
+if (isPrimo) {
+  console.log("O número é primo.");
+} else {
+  console.log("O número não é primo.");
+}
