1fa2f249171b2ed7043cfa728b42729f8e56a725
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-30 21:41:21 -0300

    Update and rename desafio01.js to variaveis.js

---
+++ JSDESAFIOS/Etapa01/variaveis.js
@@ -0,0 +1,10 @@
+
+// DESAFIO 01 - Atribua um novo valor à variável num. O código não funcionará do jeito que está. Encontre o erro e corrija-o. Execute o código corrigido.
+
+let num = 2
+let num = 5
+console.log(num)
+// Resposta
+let num = 2
+num = 5
+console.log(num)
