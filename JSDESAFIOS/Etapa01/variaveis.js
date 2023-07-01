2e26900fa679031eb813878322b9cea2c6958a9e
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-30 21:43:12 -0300

    Update variaveis.js

--- JSDESAFIOS/Etapa01/variaveis.js
+++ JSDESAFIOS/Etapa01/variaveis.js
@@ -8,3 +8,13 @@ console.log(num)
 let num = 2
 num = 5
 console.log(num)
+//******************
+
+// DESAFIO 02 Aqui declaramos uma variavel num porem nao atribuimos valor a ela, atribua o valor.
+
+let num;
+console.log(num)
+// Resposta
+let num;
+let num = 2
+console.log(num)
