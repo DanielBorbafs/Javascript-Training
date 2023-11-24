1eae0ba3c2c0a679ea4b00f6db7ddd6877c6dbd3
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-11-24 20:26:17 -0300

    Create 1_variaveis.js

---
+++ Conteudo/Nivel01/1_variaveis.js
@@ -0,0 +1,13 @@
+// Oque é uma variável?
+
+// Tipos em javascript - VAR - LET - CONST
+var idade = 23;
+let salario = 2500;
+const nome = 'daniel' 
+
+// Perigo de usar VAR
+if (true) {
+    var x = 10;
+}
+
+
