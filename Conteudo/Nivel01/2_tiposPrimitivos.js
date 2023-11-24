cf927bf8397482de55f62a8d3230457f4f3a1e85
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-11-24 20:26:35 -0300

    Create 2_tiposPrimitivos.js

---
+++ Conteudo/Nivel01/2_tiposPrimitivos.js
@@ -0,0 +1,10 @@
+let nome = 'Daniel';//string
+let salario = '1500'// string
+const salario2 = 1600; // number
+let cargo = undefined; // indefinido
+let admin = true; // boolean
+const senha = null; 
+
+
+// Tipagem dinâmica
+console.log(typeof(salario))
