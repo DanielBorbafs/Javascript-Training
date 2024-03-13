dab512f8f64db1b578fdc438ee4edb9e19ddf9ca
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-12 23:57:28 -0300

    Create aritmeticos.js

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,10 @@
+// calcular a soma de dois valores
+let valor01 = 5;
+let valor02 = 6;
+console.log(valor01 + valor02)
+// multiplicação
+console.log(valor01 * valor02)
+//divisão
+console.log(valor01 / valor02)
+//subtração 
+console.log(valor01 - valor02)
