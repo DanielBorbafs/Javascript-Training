44882dece83c04b1a7f2e0198009aa3e66330393
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-13 01:18:37 -0300

    Update aritmeticos.js

--- operadores/aritmeticos.js
+++ operadores/aritmeticos.js
@@ -8,9 +8,19 @@ console.log(valor01 * valor02)
 console.log(valor01 / valor02)
 //subtração 
 console.log(valor01 - valor02)
----------------------------------------------------
+//---------------------------------------------------
 // Calcular o perímetro de um retangulo
 let width = 120
 let height = 150
 const perimetroRetangulo = 2 * (width + height)
 console.log("O perímetro de um retângulo é: " +  perimetroRetangulo + "m²")
+//---------------------------------------------------
+//Calcule a area de um quadrado
+let lado = 12;
+const areaQuadrado = lado * lado
+console.log(areaQuadrado)
+//--------------------------------------------------
+// Calcular o perímetro de um quadrado
+let lado01 = 12;
+const perimetroQuadrado = 4 * lado01; 
+console.log("O perímetro do quadrado é " + perimetroQuadrado + " unidades");
