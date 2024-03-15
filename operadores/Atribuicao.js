bf30b5a49404956fa1dce5e35765a3d5957f9d70
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-15 03:52:54 -0300

    Create Atribuicao.js

---
+++ operadores/Atribuicao.js
@@ -0,0 +1,27 @@
+/* RESUMO */
+
+/* Atribuição simples */
+let x = 5;
+/* Atribuição de Adição (+=) */
+let y = 10
+y+= 5; // Equivale a y = y +5
+
+/* Atribuição de subtração(-=)*/
+let z = 20;
+z -= 3; // Equivalente a z = z - 3;
+
+/* Atribuição de multiplicação(*=) */
+let a = 3;
+a *= 4;
+
+/* Atribuição de Divisão (/=) */
+let b = 15;
+b /= 3
+
+// Atribuição de módulo
+let c = 10;
+c %= 3; // Equivalente a c = c % 3;
+
+// Atribuição de Exponenciação
+let d = 2;
+d **= 3; // Equivalente a d = d ** 3;
