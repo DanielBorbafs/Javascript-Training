594a17791faedd1d26e110373583c6a227813175
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-19 01:17:32 -0300

    Update intro.js

--- 01_Variaveis&Datatypes/intro.js
+++ 01_Variaveis&Datatypes/intro.js
@@ -5,3 +5,19 @@ function exemplo() {
 }
 exemplo(); // AQUI ESTAMOS EXECUTANDO A FUNÇÃO ENTÃO ELA VAI FUNCIONAR
 console.log(mensagem) // AQUI ESTAMOS TENTANDO ACESSAR DIRETAMENTE, ENTÃO IRÁ DAR ERRO.
+
+
+// LET - POSSUI ESCOPO DE BLOCO {}, SÓ CONSEGUIMOS ACESSÁ-LA DENTRO DE UM BLOCO. EX;
+function exemploLet() {
+    if (true) {
+        let mensagem = "Esta é uma mensagem dentro do escopo de bloco com let";
+    }
+    console.log(mensagem); // Erro: mensagem is not defined (mensagem não está definida aqui)
+}
+
+exemploLet(); // Erro: mensagem is not defined (mensagem não está definida fora do escopo de bloco)
+
+
+// CONST, UMA CONSTANTE ONDE UM VALOR NÃO PODE SER REATRÍBUIDO. EX:
+const salario = 1200
+salario = 1230 // erro: Cannot assign to "salario" because it is a constant.
