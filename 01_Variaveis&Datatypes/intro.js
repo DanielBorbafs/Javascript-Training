fe2e0310937186def00b0a3ceee7888182846b51
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-19 01:07:33 -0300

    Update intro.js

--- 01_Variaveis&Datatypes/intro.js
+++ 01_Variaveis&Datatypes/intro.js
@@ -1,1 +1,7 @@
-
+// VAR - POSSUI ESCOPO FUNÇÃO, CONSEGUIMOS ACESSAR APENAS DENTRO DE UMA FUNÇÃO . EX:
+function exemplo() {
+    var mensagem = "Esta é uma mensagem dentro da função";
+    console.log(mensagem);
+}
+exemplo(); // AQUI ESTAMOS EXECUTANDO A FUNÇÃO ENTÃO ELA VAI FUNCIONAR
+console.log(mensagem) // AQUI ESTAMOS TENTANDO ACESSAR DIRETAMENTE, ENTÃO IRÁ DAR ERRO.
