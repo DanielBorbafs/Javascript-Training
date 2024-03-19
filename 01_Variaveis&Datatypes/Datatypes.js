a93fa896e157f653e09e24e5c43259fdc9b212bb
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-19 01:22:53 -0300

    Create Datatypes.js

---
+++ 01_Variaveis&Datatypes/Datatypes.js
@@ -0,0 +1,10 @@
+let palavra = "isso é uma string"; // STRING
+
+let salario = 1200 //NUMBER
+
+let isAdmin = false // Boolean
+console.log(typeof(isAdmin))
+
+//Null: Representa a ausência intencional de qualquer valor, geralmente atribuído a variáveis ou propriedades.
+//Undefined: Indica que uma variável foi declarada, mas ainda não foi atribuída a um valor.
+//Symbol: Um tipo de dado novo em ECMAScript 6 (ES6) que é usado para criar identificadores únicos.
