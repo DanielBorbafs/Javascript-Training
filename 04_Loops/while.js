f6aad3eb994930ec36da409af3267b716fa06f0c
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-03-21 04:00:21 -0300

    Create while.js

---
+++ 04_Loops/while.js
@@ -0,0 +1,7 @@
+// Solicitar ao usuário uma entrada diferente de 0
+let numero = 0;
+while (numero <= 0) {
+    numero = parseInt(prompt("Digite um número positivo diferente de zero:"));
+}
+alert("Você digitou: " + numero);
+
