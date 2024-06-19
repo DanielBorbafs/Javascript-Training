def85280ae1ee4dd7a173631d2d6c56d527fa07c
DanielBorbafs <dev.danielborba@gmail.com>
2024-06-19 10:05:48 -0300

    Excluindo arquivos desnecessários

--- 04_Loops/while.js
+++
@@ -1,7 +0,0 @@
-// Solicitar ao usuário uma entrada diferente de 0
-let numero = 0;
-while (numero <= 0) {
-    numero = parseInt(prompt("Digite um número positivo diferente de zero:"));
-}
-alert("Você digitou: " + numero);
-
--- Array/exercicios-array02.js
+++
@@ -1,9 +0,0 @@
-
-// 1 some todos os números do array e divida por2
-
-const arraySoma = [10, 20, ,25, 35 ,80, 11]
-const Somatotal = arraySoma.reduce((acumulador, ValorAtual)=> {
-   return acumulador + ValorAtual;
-}, 0);
-
-console.log(Somatotal/2)
