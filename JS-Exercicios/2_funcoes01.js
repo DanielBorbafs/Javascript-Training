70467d4eeaf1856a4139dba18ac2ac6461812b96
DanielBorbafs <dev.danielborba@gmail.com>
2024-06-20 08:38:16 -0300

    function exercicio 01

---
+++ JS-Exercicios/2_funcoes01.js
@@ -0,0 +1,10 @@
+// me dê uma função que receba um parâmetro e retorne se o usuário é maior de idade ou não
+function getIdade(idade) {
+    let idadeget = idade >= 18 ? 'maior de idade' : 'menor de idade'
+    return idadeget
+}
+  
+console.log(getIdade(17));
+
+
+
