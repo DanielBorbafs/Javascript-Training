dd1e2d840e345eab426dcc2ec97fb5b10fab63a5
Danielborbafs <dev.danielborba@gmail.com>
2024-06-22 07:21:32 -0300

    Objetos exercicio 01

--- JS-Exercicios/3_objetos.js
+++ JS-Exercicios/3_objetos.js
@@ -1,1 +1,23 @@
--- criar exercícios de objetos
+// declare um objeto que possua uma função dentro dele
+
+let Carro = {
+    Marca: "Fiat",
+    Ano: 2024,
+    Modelo: "Mobi",
+    Acelero: function Acelerar() {
+        return console.log("o carro de modelo " + this.Modelo + " está acelerando")
+    }
+}
+
+Carro.Acelero()
+
+//  crie um objeto para verificar se um usuario é admin usando operador ternário
+
+let Usuario = {
+    login: "Daniel",
+    admin: true
+}
+
+let isAdmin = Usuario.admin ? "Logar como admin" : "Logar como Usuário normal";
+
+console.log(isAdmin)
