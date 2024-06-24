1d91769090f29837c1b5a4d872bb07f4f5c4f798
DanielBorbafs <dev.danielborba@gmail.com>
2024-06-24 11:33:18 -0300

    Exercicio 03 Objeto

--- JS-Exercicios/3_objetos.js
+++ JS-Exercicios/3_objetos.js
@@ -21,3 +21,24 @@ let Usuario = {
 let isAdmin = Usuario.admin ? "Logar como admin" : "Logar como Usuário normal";
 
 console.log(isAdmin)
+
+
+// Crie uma função que crie objetos 
+
+const MotoTemplate = {
+    criacao: function() {
+        return `${this.marca} ${this.modelo} ${this.ano}`
+    }
+};
+
+function criarMoto(marca, modelo, ano) {
+    const novaMoto = Object.create(MotoTemplate);
+    novaMoto.marca = marca;
+    novaMoto.modelo = modelo;
+    novaMoto.ano = ano;
+    return novaMoto;
+}
+
+
+const minhaMoto = criarMoto('Honda','Sahara 300', 2023);
+console.log(minhaMoto.criacao());
